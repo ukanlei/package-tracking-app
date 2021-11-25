14030630f79b52203eba5dfccc27c66bc1070881
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-11-25 15:32:53 -0600

    create project

---
+++ package-tracking-app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace package_tracking_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
