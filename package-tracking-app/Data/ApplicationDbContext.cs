404fde700d75adad2fe9ef4fd9e6c90ddd05c795
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-11-29 02:00:05 -0600

    Remove extra  dbcontext generated through identity

--- package-tracking-app/Data/ApplicationDbContext.cs
+++ package-tracking-app/Data/ApplicationDbContext.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using package_tracking_app.Models;
 
 namespace package_tracking_app.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public DbSet<Package> Packages{ get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
