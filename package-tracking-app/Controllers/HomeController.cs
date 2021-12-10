215c66e289560cd0b837fd3cb9a900de79c4b890
ukalelei <uvioletl@hotmail.com>
2021-12-10 06:01:17 -0600

    Add validations. User input is added to database if ModelState.IsValid is true

--- package-tracking-app/Controllers/HomeController.cs
+++ package-tracking-app/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using package_tracking_app.Data;
 using package_tracking_app.Models;
+using package_tracking_app.ViewModels;
 
 namespace package_tracking_app.Controllers
 {
@@ -15,15 +16,38 @@ namespace package_tracking_app.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbcontext)
         {
             _logger = logger;
+            context = dbcontext;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            AddPackageViewModel addPackageViewModel = new AddPackageViewModel();
+            return View(addPackageViewModel);
         }
 
+        [HttpPost]
+        public IActionResult ProcessAddForm(AddPackageViewModel addPackageViewModel)
+        {
+            //add new package info if input meets validation
+            if (ModelState.IsValid)
+            {
+                Package newPackage = new Package
+                {
+                    TrackingNumber = addPackageViewModel.TrackingNumber,
+                    Carrier = addPackageViewModel.Carrier,
+                    Description = addPackageViewModel.Description
+                };
+
+                context.Packages.Add(newPackage);
+                context.SaveChanges();
+                return RedirectToAction("Index","TrackingPackages");
+            }
+
+            return View("Index", addPackageViewModel);
+        }
     }
 }
---
+++ package-tracking-app/ViewModels/AddPackageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace package_tracking_app.ViewModels
+{
+    public class AddPackageViewModel
+    {
+        [Required(ErrorMessage = "Tracking number required")]
+        [RegularExpression (@"^[0-9]*$", ErrorMessage = "Please enter a valid tracking number")]
+        public string TrackingNumber { get; set; }
+
+        [Required(ErrorMessage = "Carrier required")]
+        public string Carrier { get; set; }
+
+        public string Description { get; set; }
+
+        public AddPackageViewModel()
+        {
+        }
+    }
+}
