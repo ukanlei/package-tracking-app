c2f4499d4860fb5f5575d36a41bc92da75076769
ukalelei <uvioletl@hotmail.com>
2021-12-10 05:48:54 -0600

    New migration. Update database

---
+++ package-tracking-app/Migrations/20211210104105_UpdatedPackageColumns.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace package_tracking_app.Migrations
+{
+    public partial class UpdatedPackageColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TrackingNumber",
+                table: "Packages",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Packages",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Packages");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TrackingNumber",
+                table: "Packages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
