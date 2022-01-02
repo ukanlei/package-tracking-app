98a2c47a58b21568437e60dd19a9cc4ac626d34b
ukalelei <uvioletl@hotmail.com>
2022-01-01 23:03:42 -0600

    Merge TrackPackageController with HomeController

--- package-tracking-app/Controllers/HomeController.cs
+++ package-tracking-app/Controllers/HomeController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using package_tracking_app.Data;
 using package_tracking_app.Models;
@@ -22,12 +22,39 @@ namespace package_tracking_app.Controllers
             context = dbcontext;
         }
 
-        [HttpGet]
-        public IActionResult Index()
+        public IActionResult AddForm()
         {
             MainModel mainModel = new MainModel();
             mainModel.AddPackageViewModel = new AddPackageViewModel();
-            //AddPackageViewModel addPackageViewModel = new AddPackageViewModel();
+            return View(mainModel);
+        }
+
+        public IActionResult Index()
+        {
+            MainModel mainModel = new MainModel();
+            mainModel.PackageList = context.Packages.ToList();
+            return View(mainModel);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            MainModel mainModel = new MainModel();
+            mainModel.Package = context.Packages.Find(id);
+            return View(mainModel);
+        }
+
+        public IActionResult Delete()
+        {
+            MainModel mainModel = new MainModel();
+            mainModel.PackageList = context.Packages.ToList();
+            return View(mainModel);
+        }
+
+        
+        public IActionResult EditName(int id)
+        {
+            MainModel mainModel = new MainModel();
+            mainModel.Package = context.Packages.Find(id);
             return View(mainModel);
         }
 
@@ -46,30 +73,89 @@ namespace package_tracking_app.Controllers
 
                 context.Packages.Add(newPackage);
                 context.SaveChanges();
-                return RedirectToAction("Index", "TrackPackages");
+                return RedirectToAction("Index");
             }
 
             return View("Index", mainModel);
         }
 
-        /*public IActionResult ProcessAddForm(AddPackageViewModel addPackageViewModel)
+
+        [HttpPost] //delete package
+        public IActionResult Delete(int[] ids)
         {
-            //add new package info if input meets validation
-            if (ModelState.IsValid)
+            MainModel mainModel = new MainModel();
+            foreach (int id in ids)
             {
-                Package newPackage = new Package
-                {
-                    TrackingNumber = addPackageViewModel.TrackingNumber,
-                    Carrier = addPackageViewModel.Carrier,
-                    Description = addPackageViewModel.Description
-                };
-
-                context.Packages.Add(newPackage);
-                context.SaveChanges();
-                return RedirectToAction("Index","TrackPackages");
+                mainModel.Package = context.Packages.Find(id);
+                context.Packages.Remove(mainModel.Package);
             }
+            context.SaveChanges();
+            return Redirect("Index");
+        }
 
-            return View("Index", addPackageViewModel);
+
+        [HttpPost] //change existign description to a new name
+        public IActionResult EditName(int id, string newName)
+        {
+            MainModel mainModel = new MainModel();
+            mainModel.Package = context.Packages.Find(id);
+            mainModel.Package.Description = newName;
+
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+        // GET: /<controller>/
+        /*public IActionResult Index()
+        {
+            List<Package> packages = context.Packages.ToList();
+            return View(packages);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Package thePackage = context.Packages.Find(id);
+            return View(thePackage);
+        }
+
+        public IActionResult Delete() 
+        {
+            List<Package> packages = context.Packages.ToList();
+            return View(packages);
+        }
+
+
+        public IActionResult EditName(int id)
+        {
+            Package thePackage = context.Packages.Find(id);
+            return View(thePackage);
+        }
+
+
+        [HttpPost] //delete package
+        public IActionResult Delete(int[] ids)
+        {
+            
+            foreach (int id in ids)
+            {
+                Package thePackage = context.Packages.Find(id);
+                context.Packages.Remove(thePackage);
+            }
+            context.SaveChanges();
+            return Redirect("Index");
+        }
+
+
+        [HttpPost] //change existign description to a new name
+        public IActionResult EditName(int id, string newName)
+        {
+            Package thePackage = context.Packages.Find(id);
+            thePackage.Description = newName;
+            
+            context.SaveChanges();
+            return RedirectToAction("Index", "TrackPackages");
         }*/
     }
 }
