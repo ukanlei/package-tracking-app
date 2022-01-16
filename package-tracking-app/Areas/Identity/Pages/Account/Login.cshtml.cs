4a167e23088b7512a1db9a3412ab718cc7513f27
ukalelei <uvioletl@hotmail.com>
2022-01-15 19:24:24 -0600

    Add styling to login and register view

--- package-tracking-app/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ package-tracking-app/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace package_tracking_app.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Remember me")]
             public bool RememberMe { get; set; }
         }
 
